5587a1afd702fcd5b9bfc9b355564bea6f0525eb
RSGC-Pritchard-M <Matthew@MacBook-Pro704-Pritchard.local>
2016-05-20 10:14:46 -0400

    Gets JSON from the internet

--- Parking/ViewController.swift
+++ Parking/ViewController.swift
@@ -7,19 +7,201 @@
 //
 
 import UIKit
+import CoreLocation
 
-class ViewController: UIViewController {
 
+class ViewController : UIViewController {
+    
+    // Views that need to be accessible to all methods
+    let jsonResult = UILabel()
+    
+    // Required to obtain location
+    var locationManager = CLLocationManager()
+    
+    // If data is successfully retrieved from the server, we can parse it here
+    func parseMyJSON(theData : NSData) {
+    
+        
+        // De-serializing JSON can throw errors, so should be inside a do-catch structure
+        do {
+            
+            // Do the initial de-serialization
+            // Source JSON is here:
+            // http://www1.toronto.ca/City%20Of%20Toronto/Information%20&%20Technology/Open%20Data/Data%20Sets/Assets/Files/greenPParking2015.json
+            //
+            let json = try NSJSONSerialization.JSONObjectWithData(theData, options: NSJSONReadingOptions.AllowFragments) as! AnyObject
+            
+
+            print(json)
+            
+            
+            
+            
+
+            
+            // Now we can update the UI
+            // (must be done asynchronously)
+            dispatch_async(dispatch_get_main_queue()) {
+                
+                //self.jsonResult.text = "parsed JSON should go here"
+                
+                // Create a space in memory to store the current location
+                //var currentLocation = CLLocation()
+                
+                
+                
+                
+                
+            }
+            
+            
+        } catch let error as NSError {
+            print ("Failed to load: \(error.localizedDescription)")
+        }
+        
+        
+    }
+    
+    // Set up and begin an asynchronous request for JSON data
+    func getMyJSON() {
+        
+        // Define a completion handler
+        // The completion handler is what gets called when this **asynchronous** network request is completed.
+        // This is where we'd process the JSON retrieved
+        let myCompletionHandler : (NSData?, NSURLResponse?, NSError?) -> Void = {
+            
+            (data, response, error) in
+            
+
+            
+            // Cast the NSURLResponse object into an NSHTTPURLResponse objecct
+            if let r = response as? NSHTTPURLResponse {
+                
+                // If the request was successful, parse the given data
+                if r.statusCode == 200 {
+                    
+                    if let d = data {
+                        
+                        // Parse the retrieved data
+                        self.parseMyJSON(d)
+                        
+                    }
+                    
+                }
+                
+            }
+            
+        }
+        
+        // Define a URL to retrieve a JSON file from
+        let address : String = "http://www1.toronto.ca/City%20Of%20Toronto/Information%20&%20Technology/Open%20Data/Data%20Sets/Assets/Files/greenPParking2015.json"
+        
+        // Try to make a URL request object
+        if let url = NSURL(string: address) {
+            
+            // We have an valid URL to work with
+            print(url)
+            
+            // Now we create a URL request object
+            let urlRequest = NSURLRequest(URL: url)
+            
+            // Now we need to create an NSURLSession object to send the request to the server
+            let config = NSURLSessionConfiguration.defaultSessionConfiguration()
+            let session = NSURLSession(configuration: config)
+            
+            // Now we create the data task and specify the completion handler
+            let task = session.dataTaskWithRequest(urlRequest, completionHandler: myCompletionHandler)
+            
+            // Finally, we tell the task to start (despite the fact that the method is named "resume")
+            task.resume()
+            
+        } else {
+            
+            // The NSURL object could not be created
+            print("Error: Cannot create the NSURL object.")
+            
+        }
+        
+    }
+    
+    // This is the method that will run as soon as the view controller is created
     override func viewDidLoad() {
+        
+        // Sub-classes of UIViewController must invoke the superclass method viewDidLoad in their
+        // own version of viewDidLoad()
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // Make the view's background be white
+        // Trying to match colours expected on iOS
+        // http://iosdesign.ivomynttinen.com/#color-palette
+        view.backgroundColor = UIColor.whiteColor()
+        
+        /*
+        * Further define label that will show JSON data
+        */
+        
+        // Set the label text and appearance
+        jsonResult.text = "..."
+        jsonResult.font = UIFont.systemFontOfSize(12)
+        jsonResult.numberOfLines = 0   // makes number of lines dynamic
+        // e.g.: multiple lines will show up
+        jsonResult.textAlignment = NSTextAlignment.Center
+        
+        // Required to autolayout this label
+        jsonResult.translatesAutoresizingMaskIntoConstraints = false
+        
+        // Add the label to the superview
+        view.addSubview(jsonResult)
+        
+        /*
+        * Add a button
+        */
+        let getData = UIButton(frame: CGRect(x: 0, y: 0, width: 150, height: 30))
+        
+        // Make the button, when touched, run the calculate method
+        getData.addTarget(self, action: "getMyJSON", forControlEvents: UIControlEvents.TouchUpInside)
+        
+        // Set the button's title
+        getData.setTitle("Get my JSON!", forState: UIControlState.Normal)
+        
+        // Set the button's color
+        getData.setTitleColor(UIColor.init(red: 0.329, green: 0.78, blue: 0.988, alpha: 1), forState: UIControlState.Normal)
+        
+        // Required to auto layout this button
+        getData.translatesAutoresizingMaskIntoConstraints = false
+        
+        // Add the button into the super view
+        view.addSubview(getData)
+        
+        /*
+        * Layout all the interface elements
+        */
+        
+        // This is required to lay out the interface elements
+        view.translatesAutoresizingMaskIntoConstraints = false
+        
+        // Create an empty list of constraints
+        var allConstraints = [NSLayoutConstraint]()
+        
+        // Create a dictionary of views that will be used in the layout constraints defined below
+        let viewsDictionary : [String : AnyObject] = [
+            "title": jsonResult,
+            "getData": getData]
+        
+        // Define the vertical constraints
+        let verticalConstraints = NSLayoutConstraint.constraintsWithVisualFormat(
+            "V:|-50-[getData]-[title]",
+            options: [],
+            metrics: nil,
+            views: viewsDictionary)
+        
+        // Add the vertical constraints to the list of constraints
+        allConstraints += verticalConstraints
+        
+        // Activate all defined constraints
+        NSLayoutConstraint.activateConstraints(allConstraints)
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
+    
 }
 
